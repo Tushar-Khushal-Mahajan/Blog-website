dfd732303a791abe594f59742e4246cf689ffb1d
Tushar Khushal Mahajan <mahajantushar208@gmail.com>
2024-06-29 18:00:30 +0530

    working with all post dynamic data

---
+++ service/post-service.js
@@ -0,0 +1,174 @@
+var post_data = [
+    {
+        uid: "1",
+        u_name: "tushar",
+        password: "123",
+
+        posts: [
+            {
+                p_id: "1",
+                title: "what is programming",
+                content: "This is a longer card with supporting text below as a natural lead-in to..",
+                date: new Date().getDate(),
+                img: "images/blog.jpg",
+
+                comment: [
+                    {
+                        c_id: "1",
+                        desc: "nice",
+                        date: new Date().getDate()
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        uid: "2",
+        u_name: "jayesh",
+        password: "123",
+
+        posts: [
+            {
+                p_id: "2",
+                title: "what is programming",
+                content: "This is a longer card with supporting text below as a natural lead-in to..",
+                date: new Date().getDate(),
+                img: "images/blog.jpg",
+                comment: [
+                    {
+                        c_id: "2",
+                        desc: "nice",
+                        date: new Date().getDate()
+                    }
+                ]
+            }
+        ],
+    },
+    {
+        uid: "3",
+        u_name: "bhuvan",
+        password: "123",
+
+        posts: [
+            {
+                p_id: "3",
+                title: "what is programming",
+                content: "This is a longer card with supporting text below as a natural lead-in to..",
+                date: new Date().getDate(),
+                img: "images/blog.jpg",
+                comment: [
+                    {
+                        c_id: "3",
+                        desc: "nice",
+                        date: new Date().getDate()
+                    }
+                ]
+            },
+            {
+                p_id: "4",
+                title: "what is java",
+                content: "This is a longer card with supporting text below as a natural lead-in to..",
+                date: new Date().getDate(),
+                img: "images/blog.jpg",
+                comment: [
+                    {
+                        c_id: "4",
+                        desc: "nice",
+                        date: new Date().getDate()
+                    }
+                ]
+            }
+        ],
+    },
+    {
+        uid: "4",
+        u_name: "vaibhav",
+        password: "123",
+
+        posts: [
+            {
+                p_id: "5",
+                title: "what is programming",
+                content: "This is a longer card with supporting text below as a natural lead-in to..",
+                date: new Date().getDate(),
+                img: "images/blog.jpg",
+                comment: [
+                    {
+                        c_id: "5",
+                        desc: "nice",
+                        date: new Date().getDate()
+                    }
+                ]
+            },
+            {
+                p_id: "6",
+                title: "what is java",
+                content: "This is a longer card with supporting text below as a natural lead-in to..",
+                date: new Date().getDate(),
+                img: "images/blog.jpg",
+                comment: [
+                    {
+                        c_id: "6",
+                        desc: "nice",
+                        date: new Date().getDate()
+                    }
+                ]
+            }
+        ],
+    },
+    {
+        uid: "5",
+        u_name: "tushar",
+        password: "123",
+
+        posts: [
+            {
+                p_id: "7",
+                title: "what is programming",
+                content: "This is a longer card with supporting text below as a natural lead-in to..",
+                date: new Date().getDate(),
+                img: "images/blog.jpg",
+                comment: [
+                    {
+                        c_id: "7",
+                        desc: "nice",
+                        date: new Date().getDate()
+                    }
+                ]
+            },
+            {
+                p_id: "8",
+                title: "what is java",
+                content: "This is a longer card with supporting text below as a natural lead-in to..",
+                date: new Date().getDate(),
+                img: "images/blog.jpg",
+                comment: [
+                    {
+                        c_id: "8",
+                        desc: "nice",
+                        date: new Date().getDate()
+                    },
+                    {
+                        c_id: "9",
+                        desc: "nice",
+                        date: new Date().getDate()
+                    }
+                ]
+            }
+        ],
+    }
+];
+
+// let getAllPosts = function () {
+//     let post_array = new Array();
+
+//     for (data of post_data) {
+//         posts.push(postObj = {
+//             u_name: data.u_name,
+//             posts: data.posts,
+//             comments: data.comment
+//         });
+
+//         return post_array;
+//     }
+// }
